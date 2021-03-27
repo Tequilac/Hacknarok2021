2e5b65543c5c6ce733f82b27484c51377f404e55
Kamilbur <kamil.burkiewicz@gmail.com>
2021-03-27 20:44:06 +0100

    on click menu XD

--- src/menu/menu.py
+++ src/menu/menu.py
@@ -1,85 +1,59 @@
 import sys
 import pygame
 
-from settings import MENU_FONT, Colors
+from .menu_button import MenuButton
+from settings import Colors
 
 
 class Menu:
     BUTTONS_INIT_HEIGHT = 200
     BUTTONS_DIST = 10
-    BUTTONS_NAMES = [
-        'START',
-        'QUIT',
-    ]
 
     def __init__(self, width):
         self._running = True
         self.current_menu_option = 0
 
         self.width = width
-        self.buttons = {
-            self.BUTTONS_NAMES[0]: MENU_FONT.render(self.BUTTONS_NAMES[0],
-                                                    True,
-                                                    Colors.BLACK.value,
-                                                    Colors.VERY_LIGHT_BLUE.value),
-            self.BUTTONS_NAMES[1]: MENU_FONT.render(self.BUTTONS_NAMES[1],
-                                                    True,
-                                                    Colors.BLACK.value,
-                                                    Colors.VERY_LIGHT_BLUE.value),
-        }
-        self.buttons_num = len(self.BUTTONS_NAMES)
-        self.buttons_heights = []
-        height_sum = self.BUTTONS_INIT_HEIGHT
-        for i, button_name in enumerate(self.buttons.values()):
-            button_name = self.buttons[self.BUTTONS_NAMES[i]]
+        self.buttons = pygame.sprite.Group()
 
-            self.buttons_heights += [height_sum]
-            height_sum += button_name.get_height() + self.BUTTONS_DIST
+        button_names = [
+            'START',
+            'QUIT',
+        ]
+
+        height = self.BUTTONS_INIT_HEIGHT
+        for button_name in button_names:
+            button = MenuButton(button_name, Colors.WHITE.value, 200, 100, button_name)
+            button.rect.center = (self.width // 2, height)
+            height += self.BUTTONS_DIST + button.rect.height
+            self.buttons.add(button)
 
     def initialize(self, app_surface):
         app_surface.fill(Colors.BLUE.value)
-        self.draw_buttons(app_surface)
-        pygame.display.update()
 
-    def draw_buttons(self, app_surface):
-        height_sum = self.BUTTONS_INIT_HEIGHT
-
-        for i, button_name in enumerate(self.buttons.values()):
-            app_surface.blit(button_name, ((self.width - button_name.get_width()) // 2, height_sum))
-            height_sum += button_name.get_height() + self.BUTTONS_DIST
-
-    def on_render(self, app_surface):
-        app_surface.fill(Colors.BLUE.value)
-        current_button = self.buttons[self.BUTTONS_NAMES[self.current_menu_option]]
-        left_cord = 0
-        top_cord = self.buttons_heights[self.current_menu_option]
-        rectangle = pygame.Rect((left_cord, top_cord), (self.width, current_button.get_height()))
-        pygame.draw.rect(app_surface, Colors.VERY_LIGHT_BLUE.value, rectangle)
-        self.draw_buttons(app_surface)
+    def on_render(self):
         pygame.display.update()
 
     def on_event(self, event):
         if event.type == pygame.QUIT:
             sys.exit(0)
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN:
-                self.current_menu_option = (self.current_menu_option - 1) % self.buttons_num
-            elif event.key == pygame.K_UP:
-                self.current_menu_option = (self.current_menu_option + 1) % self.buttons_num
-            elif event.key == pygame.K_RETURN:
-                self.action_perform()
-
-    def action_perform(self):
-        if self.BUTTONS_NAMES[self.current_menu_option] == 'START':
-            self._running = False
-        elif self.BUTTONS_NAMES[self.current_menu_option] == 'QUIT':
-            sys.exit(0)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            mouse_sprite = pygame.sprite.Sprite()
+            mouse_sprite.rect = pygame.rect.Rect((x, y), (1, 1))
+            for button in self.buttons:
+                if pygame.sprite.collide_rect(button, mouse_sprite):
+                    if button.name == 'START':
+                        self._running = False
+                    elif button.name == 'QUIT':
+                        sys.exit(0)
 
     def run(self, app_surface):
         self.initialize(app_surface)
 
         while self._running:
+            self.buttons.draw(app_surface)
             for event in pygame.event.get():
                 self.on_event(event)
 
-            self.on_render(app_surface)
+            self.on_render()
---
+++ src/menu/menu_button.py
@@ -0,0 +1,15 @@
+import pygame
+import settings
+
+
+class MenuButton(pygame.sprite.Sprite):
+    def __init__(self, text, color, width, height, name):
+        super().__init__()
+        self.font = settings.MENU_FONT
+        self.name = name
+        self.textSurf = self.font.render(text, True, color)
+        self.image = pygame.Surface((width, height))
+        self.rect = self.image.get_rect()
+        w = self.textSurf.get_width()
+        h = self.textSurf.get_height()
+        self.image.blit(self.textSurf, (width / 2 - w / 2, height / 2 - h / 2))
