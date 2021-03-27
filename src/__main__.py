cbc175e5a02dbe0afc9b89b29ea76746f4f04196
Kamilbur <kamil.burkiewicz@gmail.com>
2021-03-27 14:55:41 +0100

    menu prototype

--- src/__main__.py
+++ src/__main__.py
@@ -1,21 +1,36 @@
 import pygame
-from pygame.locals import *
+import menu
+import settings
+
+pygame.font.init()
 
 
 class App:
+    GAME_FONT = pygame.font.SysFont("Arial.ttf", 24)
+
     def __init__(self):
         self._running = True
         self._display_surf = None
         self.size = self.weight, self.height = 640, 400
+        self.menu = menu.Menu()
 
     def on_init(self):
         pygame.init()
         self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self._running = True
 
+        self._display_surf.fill(settings.Colors.BLUE.value)
+        textsurface = self.GAME_FONT.render('Menu XD', False, settings.Colors.WHITE.value)
+        self._display_surf.blit(textsurface, (0, 0))
+        pygame.display.update()
+
     def on_event(self, event):
         if event.type == pygame.QUIT:
             self._running = False
+        if event.type == pygame.KEYDOWN:
+            pass
+        if event.type == pygame.KEYUP:
+            pass
 
     def on_loop(self):
         pass
@@ -27,14 +42,14 @@ class App:
         pygame.quit()
 
     def on_execute(self):
-        if self.on_init() == False:
-            self._running = False
+        self.on_init()
 
-        while (self._running):
+        while self._running:
             for event in pygame.event.get():
                 self.on_event(event)
             self.on_loop()
             self.on_render()
+
         self.on_cleanup()
 
 
